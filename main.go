01a99ba21e0e2cbee9d18f7c07e2f8dfc8bee719
Judah Suryaputra <judahsuryaputra@gmail.com>
2021-09-10 18:07:35 +0700

    add case insensitive sort

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 )
 
 type Cart map[string]int
@@ -13,17 +15,35 @@ func main() {
     cart.addProduct("Pisang Hijau", 2)
 
     cart.addProduct("Semangka Kuning", 3)
+	
+    cart.addProduct("pisang merah", 3)
 
     cart.addProduct("Apel Merah", 1)
     cart.addProduct("Apel Merah", 4)
     cart.addProduct("Apel Merah", 2)
 
-    cart.deleteProduct("Semangka Kuning")
+    // cart.deleteProduct("Semangka Kuning")
     
     cart.deleteProduct("Semangka Merah")
     
-    cart.showCart()
+	cart.orderCart()
+    // cart.showCart()
 
+}
+
+// order the products on Cart case-insensitively
+func (cart Cart) orderCart() {
+	keys := make([]string, 0)
+	for k, _ := range cart {
+		keys = append(keys, k)
+	}
+	// sort.Strings(keys) // Case sensitive sorting
+	sort.Slice(keys, func(i, j int) bool { 
+			return strings.ToLower(keys[i]) < strings.ToLower(keys[j])})
+
+	for _, k := range keys {
+		fmt.Println(k, cart[k])
+	}
 }
 
 func (cart Cart) newCart() Cart {
@@ -33,7 +53,7 @@ func (cart Cart) newCart() Cart {
 func (cart Cart) addProduct(productCode string, quantity int){
     if _, exist := cart[productCode]; !exist {
         cart[productCode] = quantity
-    } else if _, exist := cart[productCode]; exist {
+    } else {
         cart[productCode] = cart[productCode] + quantity
     }
 
